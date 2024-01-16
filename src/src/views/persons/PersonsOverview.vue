c39710bbd7659e75ed5df5b79a1915ee8f7985f4
alessioC42 <84250128+alessioC42@users.noreply.github.com>
2024-01-16 16:03:17 +0100

    Add delete user support

--- src/src/views/persons/PersonsOverview.vue
+++ src/src/views/persons/PersonsOverview.vue
@@ -14,18 +14,28 @@
         :disable-sort="true"
         item-value="name"
         @update:options="loadItems"
-    ></v-data-table-server>
+    >
+        <template v-slot:[`item.actions`]="props">
+            <v-btn style="margin: 3px;" color="primary" @click="editItem(props.item)">
+                <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn style="margin: 3px;" color="error" @click="deleteItem(props.item)">
+                <v-icon>mdi-delete</v-icon>
+            </v-btn>
+        </template>
+    </v-data-table-server>
 </template>
 <script>
 export default {
     data: () => ({
     itemsPerPage: 10,
     headers: [
-        { title: 'username', align: 'start', key: 'username', sortable: false, },
+        { title: 'username', align: 'start', key: 'username', sortable: false },
         { title: 'First Name', key: 'first_name', align: 'end', sortable: false },
         { title: 'Second Name', key: 'second_name', align: 'end', sortable: false },
         { title: 'E-Mail', key: 'email', align: 'end', sortable: false },
-        { title: 'Created', key: 'created_at', align: 'end', sortable: false, },
+        { title: 'Created', key: 'created_at', align: 'end', sortable: false },
+        { title: 'Actions', value: 'actions', sortable: false, align: 'end' },
     ],
     search: '',
     serverItems: [],
@@ -33,16 +43,30 @@ export default {
     totalItems: 0,
     }),
     methods: {
-    loadItems ({ page, itemsPerPage, _sortBy }) {
-        this.loading = true
-        fetch(`http://localhost:3000/api/persons?page=${page}&itemsPerPage=${itemsPerPage}`).then((response) => {
-        response.json().then((data) => {
-            this.serverItems = data.results
-            this.totalItems = data.count
-            this.loading = false
-        })
-        })
-    },
+        loadItems ({ page, itemsPerPage, _sortBy }) {
+            this.loading = true
+            fetch(`http://localhost:3000/api/persons?page=${page}&itemsPerPage=${itemsPerPage}`).then((response) => {
+            response.json().then((data) => {
+                this.serverItems = data.results
+                this.totalItems = data.count
+                this.loading = false
+            })
+            })
+        },
+        editItem(item) {
+            console.log(item);
+        },
+        deleteItem(item) {
+        if (window.confirm('Are you sure you want to delete this user?')) {
+            fetch("http://localhost:3000/api/person/" + item.id, {
+                method: "DELETE",
+            }).then(response => {
+                if (response.status === 200) {
+                    this.loadItems({ page: 1, itemsPerPage: 10, _sortBy: null });
+                }
+            });
+        }
+},
     },
 }
 </script>
